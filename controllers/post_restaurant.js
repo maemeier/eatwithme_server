2ec48963bf6f8d9cea74f50d14f5c5a74a91f924
maemeier <maemeier@live.com>
2019-07-25 01:15:57 +0700

    update server

--- controllers/post_restaurant.js
+++ controllers/post_restaurant.js
@@ -1,29 +1,107 @@
 const db_message = require('../models/restaurant')
 const jwt = require('jsonwebtoken')
 const path = require('path')
-// const cloudinary = require('cloudinary')
-// const Datauri = require('datauri')
+const cloudinary = require('cloudinary')
+const Datauri = require('datauri')
 
+const data_uri = new Datauri()
+
+const create_uri = (image) => {
+	return data_uri.format(path.extname(image.originalname).toString(), image.buffer)
+}
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_NAME,
+	api_key: process.env.CLOUDINARY_KEY,
+	api_secret: process.env.CLOUDINARY_SECRET,
+})
+
+// upload
+
+const upload_file = (image) => {
+	return new Promise(function(resolve, reject) {
+		let uri = create_uri(image).content
+		cloudinary.uploader.upload(uri).then((saved) => {
+			console.log('saved', saved)
+			resolve(saved)
+		}).catch((err) => {
+			console.log('err', err)
+			reject(err)
+		})
+	})
+}
+
+const create_message = (body) => {
+	return new Promise(function(resolve, reject) {
+		db_message.create(body).then((data) => {
+			db_message.findById(data._id)
+				.populate({
+					path: 'author',
+					select: 'name email'
+				}).then((message) => {
+					resolve(message)
+				}).catch((err) => {
+					reject(err)
+				})
+		}).catch((err) => {
+			reject(err)
+		})
+	})
+}
 
 module.exports = (req, res) => {
+	// token
 	let token = req.headers.authorization.split(' ')[1]
-	jwt.verify(token, 'fyni', (err, decoded) => {
+	jwt.verify(token, process.env.SECRET, (err, decoded) => {
 		if (decoded) {
-			console.log('decoded', decoded)
 			req.body.author = decoded._id
-			db_message.create(req.body).then((data) => {
-				db_message.findById(data._id)
-					.populate({
-						path: 'author',
-						select: 'name email'
-					}).then((message) => {
+			// file
+			if (req.file && req.file != null) {
+				upload_file(req.file).then((file) => {
+					req.body.file = file.url
+					// message with file
+					create_message(req.body).then((message) => {
 						res.send(message)
 					}).catch((err) => {
 						res.send(err)
 					})
-			}).catch((err) => {
-				res.send(err)
-			})
+				}).catch((err) => {
+					res.send(err)
+				})
+			} else {
+				// message no file
+				delete req.body.file
+				console.log('req.body', req.body)
+				create_message(req.body).then((message) => {
+					res.send(message)
+				}).catch((err) => {
+					res.send(err)
+				})
+			}
 		}
 	})
 }
+
+//
+// module.exports = (req, res) => {
+// 	let token = req.headers.authorization.split(' ')[1]
+// 	jwt.verify(token, 'fyni', (err, decoded) => {
+// 		if (decoded) {
+// 			console.log('decoded', decoded)
+// 			req.body.author = decoded._id
+// 			db_message.create(req.body).then((data) => {
+// 				db_message.findById(data._id)
+// 					.populate({
+// 						path: 'author',
+// 						select: 'name email'
+// 					}).then((message) => {
+// 						res.send(message)
+// 					}).catch((err) => {
+// 						res.send(err)
+// 					})
+// 			}).catch((err) => {
+// 				res.send(err)
+// 			})
+// 		}
+// 	})
+// }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require ('cors')
+const multer = require('multer')
 require ('dotenv').config()
 
 const app = express()
@@ -16,13 +17,14 @@ app.get('/api/user', require('./controllers/get_user'))
 app.post('/api/login', require('./controllers/login'))
 app.post('/api/signup', require('./controllers/signup'))
 
-app.post('/api/event', require('./controllers/post_event'))
+app.post('/api/event',multer({ storage: multer.memoryStorage() }).single('file'), require('./controllers/post_event'))
 app.get('/api/getEvent', require('./controllers/get_event'))
 
 app.post('/api/restaurant', require('./controllers/post_restaurant'))
 app.get('/api/getRestaurant', require('./controllers/get_restaurant'))
 
 app.get('/api/getanEvent/:id', require('./controllers/get_anEvent'))
+app.get('/api/getanRestaurant/:id', require('./controllers/get_anRestaurant'))
 
 
 app.listen(process.env.PORT,(err)=>{
