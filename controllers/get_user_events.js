9915837dd58a48897db0af932557fd43b1acb50d
maemeier <maemeier@live.com>
2019-08-05 01:02:31 +0700

    added log

--- controllers/get_user_events.js
+++ controllers/get_user_events.js
@@ -15,8 +15,10 @@ module.exports = (req,res)=>{
 				path: 'author',
 				select: 'name email'
 			}).then((data)=>{
-				//for each loop of events to find where author = decoded id then push to an array
-				//for each loop of events where guest === decoded id then push to the same array 
+				console.log('data',data);
+				//to populate guests names, do another find for users with the ids of the guests in a map to get the names and then send the names with the data.
+				//for each loop of events to find where author === decoded id then push to an array
+				//for each loop of events where guest === decoded id then push to the same array
 				res.send(data)
 			}).catch((err) => {
 				res.send(err)
