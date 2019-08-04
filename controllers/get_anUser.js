401a38f3990bc9028d1af429aa505eeea6ea3d67
maemeier <maemeier@live.com>
2019-08-04 23:56:07 +0700

    logs added

--- controllers/get_anUser.js
+++ controllers/get_anUser.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res) => {
   let token = req.headers.authorization.split(" ")[1];
+	console.log('token', token);
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
     console.log("decoded get_anUser", decoded);
     db_user
