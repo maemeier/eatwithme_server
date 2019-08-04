79bed227975954c22589bd4c975c4061e983f615
maemeier <maemeier@live.com>
2019-08-05 01:58:25 +0700

    tom not smart

--- controllers/get_user_events.js
+++ controllers/get_user_events.js
@@ -8,16 +8,14 @@ module.exports = (req,res)=>{
 	jwt.verify(token, process.env.SECRET, (err, decoded) => {
 		if (decoded) {
 			//query to find only that users events
-			db_message.find({author: decoded._id}).sort('-date').populate({
+			db_message.find({guests: decoded._id}).sort('-date').populate({
 				path: 'channel',
 				select: 'author'
 			}).populate({
 				path: 'author',
 				select: 'name email'
 			}).then((data)=>{
-				//to populate guests names, do another find for users with the ids of the guests in a map to get the names and then send the names with the data.
-				//for each loop of events to find where author === decoded id then push to an array
-				//for each loop of events where guest === decoded id then push to the same array
+
 				res.send(data)
 			}).catch((err) => {
 				res.send(err)
