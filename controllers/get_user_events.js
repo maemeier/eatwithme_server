61bfbfcebb0f02d3b22cbc344870a39e0e3219c9
maemeier <maemeier@live.com>
2019-08-05 00:46:55 +0700

    added notes

--- controllers/get_user_events.js
+++ controllers/get_user_events.js
@@ -2,11 +2,12 @@ const db_message = require('../models/event')
 const jwt = require("jsonwebtoken");
 
 module.exports = (req,res)=>{
-
+//authentication
 	let token = req.headers.authorization.split(' ')[1]
 	console.log(token)
 	jwt.verify(token, process.env.SECRET, (err, decoded) => {
 		if (decoded) {
+			//query to find only that users events
 			db_message.find({author: decoded._id}).sort('-date').populate({
 				path: 'channel',
 				select: 'author'
@@ -14,6 +15,8 @@ module.exports = (req,res)=>{
 				path: 'author',
 				select: 'name email'
 			}).then((data)=>{
+				//for each loop of events to find where author = decoded id then push to an array
+				//for each loop of events where guest === decoded id then push to the same array 
 				res.send(data)
 			}).catch((err) => {
 				res.send(err)
